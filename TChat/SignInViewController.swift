4f718f4d69b4f28e0cedc9ba6093a2655af0a41a
Artsiom Sarychev <sarychevas4@gmail.com>
2021-04-27 01:20:55 +0300

    Design Sign In ViewController

---
+++ TChat/SignInViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignInViewController.swift
+//  TChat
+//
+//  Created by Артем Сарычев on 27.04.21.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var titleTextLabel: UILabel!
+    
+    
+    @IBOutlet weak var emailContainerView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordContainerView: UIView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    
+    func setupUI(){
+        
+        setupTitleLabel()
+        setupEmailTextField()
+        setupPasswordTextField()
+        setupSignUpButton()
+        setupSignInButton()
+        
+    }
+
+}
