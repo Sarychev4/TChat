998d077e5a72a58eb0cf4389c80b1d50a37b05eb
Artsiom Sarychev <sarychevas4@gmail.com>
2021-04-27 15:47:45 +0300

    Upload Profile Image to Fire Base

--- TChat/SignUpViewController.swift
+++ TChat/SignUpViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
 class SignUpViewController: UIViewController {
-
+    
     @IBOutlet weak var titleTextLabel: UILabel!
     @IBOutlet weak var avatar: UIImageView!
     
@@ -26,9 +27,11 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var signInButton: UIButton!
     
+    var image: UIImage? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUI()
     }
     
@@ -43,7 +46,7 @@ class SignUpViewController: UIViewController {
         setupSignInButton()
         
     }
-
+    
     @IBAction func dissmissAction(_ sender: Any) {
         
         navigationController?.popViewController(animated: true)
@@ -51,28 +54,63 @@ class SignUpViewController: UIViewController {
     
     //MARK: - Auth with FireBase
     @IBAction func signUpButtonDidTapped(_ sender: Any) {
-        Auth.auth().createUser(withEmail: "test2@gmail.com", password: "123456") { (authDataResult, error) in
+        
+        guard let imageSelected = self.image else {
+            print("Avatar is nil")
+            return
+        }
+        //Convert data
+        guard let imageData = imageSelected.jpegData(compressionQuality: 0.4) else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: "test5@gmail.com", password: "123456") { (authDataResult, error) in
             if error != nil {
                 print(error!.localizedDescription)
                 return
             }
             if let authData = authDataResult {
                 print(authData.user.email ?? "Empty")
-                let dict: Dictionary<String, Any> = [
+                var dict: Dictionary<String, Any> = [
                     "uid": authData.user.uid,
                     "email": authData.user.email ?? "Empty",
                     "profileImageUrl": "",
                     "status": "Default status"
                 ]
                 
-                Database.database().reference().child("users")
-                    .child(authData.user.uid)
-                    .updateChildValues(dict, withCompletionBlock: {
-                        (error, ref) in
-                        if error == nil {
-                            print("Done")
+                let storageRef = Storage.storage().reference(forURL: "gs://tchat-8865f.appspot.com")
+                let storageProfileRef = storageRef.child("profile").child(authData.user.uid)
+                
+                //Put Data in storage
+                let metadata = StorageMetadata()
+                metadata.contentType = "image/jpg"
+                //Put Data in storage
+                storageProfileRef.putData(imageData, metadata: metadata) { (storageMetaData, error) in
+                    if error != nil {
+                        print(error?.localizedDescription)
+                        return
+                    }
+                    
+                    //Download image to upload in FireBase Database
+                    storageProfileRef.downloadURL(completion: { (url, error) in
+                        if let metaImageUrl = url?.absoluteString { //Convert to String
+                            dict["profileImageUrl"] = metaImageUrl
+                            
+                            
+                            Database.database().reference().child("users")
+                                .child(authData.user.uid)
+                                .updateChildValues(dict, withCompletionBlock: {
+                                    (error, ref) in
+                                    if error == nil {
+                                        print("Done")
+                                    }
+                                })
+                            
                         }
                     })
+                }
+                
+                
             }
         }
     }
