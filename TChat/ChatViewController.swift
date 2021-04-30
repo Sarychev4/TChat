7f7de5a8bf4dbf25e0fe099480128235195a8681
Artsiom Sarychev <sarychevas4@gmail.com>
2021-04-30 17:40:39 +0300

    Design Input Message View Part 2

--- TChat/ChatViewController.swift
+++ TChat/ChatViewController.swift
@@ -9,15 +9,58 @@ import UIKit
 
 class ChatViewController: UIViewController {
     
+    @IBOutlet weak var mediaButton: UIButton!
+    @IBOutlet weak var audioButton: UIButton!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
     var imagePartner: UIImage! // image from users VC
     var avatarImageView: UIImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 36, height: 36))
     var topLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
     var partnerUsername: String!
+    var placeholderLbl = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        setupInputContainer()
         // Do any additional setup after loading the view.
+    }
+    
+    func setupInputContainer(){
+        let mediaImg = UIImage(named: "attachment_icon")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        mediaButton.setImage(mediaImg, for: .normal)
+        mediaButton.tintColor = .lightGray
+        
+        let micImg = UIImage(named: "mic")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        audioButton.setImage(micImg, for: .normal)
+        audioButton.tintColor = .lightGray
+        
+        setupInputTextView()
+    }
+    
+    func setupInputTextView(){
+        
+        inputTextView.delegate = self
+        
+        placeholderLbl.isHidden = false
+        
+        let placeholderX: CGFloat = self.view.frame.size.width / 75
+        let placeholderY: CGFloat = 0
+        let placeholderWidth: CGFloat = inputTextView.bounds.width - placeholderX
+        let placeholderHeight: CGFloat = inputTextView.bounds.height
+        
+        let placeholderFontSize = self.view.frame.size.width / 25
+        
+        placeholderLbl.frame = CGRect(x: placeholderX, y: placeholderY, width: placeholderWidth, height: placeholderHeight)
+        placeholderLbl.text = "Write a message"
+        placeholderLbl.font = UIFont(name: "HelveticaNeue", size: placeholderFontSize)
+        placeholderLbl.textColor = .lightGray
+        placeholderLbl.textAlignment = .left
+        
+        inputTextView.addSubview(placeholderLbl)
+        
+        
     }
     
     func setupNavigationBar(){
@@ -56,6 +99,8 @@ class ChatViewController: UIViewController {
         self.tabBarController?.tabBar.isHidden = false
     }
 
+    @IBAction func sendButtonDidTapped(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
@@ -66,4 +111,21 @@ class ChatViewController: UIViewController {
     }
     */
 
+}
+
+extension ChatViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let spacing = CharacterSet.whitespacesAndNewlines
+        if !textView.text.trimmingCharacters(in: spacing).isEmpty{
+            let text = textView.text.trimmingCharacters(in: spacing)
+            sendButton.isEnabled = true
+            sendButton.setTitleColor(.black, for: .normal)
+            placeholderLbl.isHidden = true
+        } else {
+            sendButton.isEnabled = false
+            sendButton.setTitleColor(.lightGray, for: .normal)
+            placeholderLbl.isHidden = false
+        }
+    }
 }
